2c8cd3dda70c1343a8975ec051711dc44612889f
Ricardo <ricardo.luo@icloud.com>
2021-02-23 09:54:13 +0800

    feat: 新增字符串自动省略号组件

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,6 @@
 import GlobalHeader from '@/components/GlobalHeader'
 import PageHeader from '@/components/PageHeader'
 import SideMenu from '@/components/Menu/SideMenu'
-import SForm from '@/components/SearchForm'
 import STable from '@/components/Table'
-export { GlobalHeader, PageHeader, SideMenu, SForm, STable }
+import Ellipsis from '@/components/Ellipsis'
+export { GlobalHeader, PageHeader, SideMenu, STable, Ellipsis }
